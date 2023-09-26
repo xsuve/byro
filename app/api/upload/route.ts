d26efda601594d41da4000922d75214558e57ff4
bw-george <george@buildwell.app>
2023-09-26 14:17:22 +0300

    Initial commit.

---
+++ app/api/upload/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { createWorker } from 'tesseract.js';
+import sharp from 'sharp';
+import path from 'path';
+
+export async function POST(request: NextRequest) {
+  const formData = await request.formData();
+  const file: File | null = formData.get('document') as unknown as File;
+
+  if (!file) {
+    return NextResponse.json({ success: false, data: null });
+  }
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  try {
+    const documentBuffer = await sharp(buffer)
+      .resize({
+        width: 500,
+        fit: sharp.fit.inside,
+      })
+      .negate()
+      .threshold()
+      .toBuffer();
+
+    const worker = await createWorker({
+      // langPath: path.join(__dirname, '..', 'ocr', 'langs'),
+      logger: (m) => {
+        // console.clear();
+        // console.log(`Loading...${Math.round(m.progress * 100)}%`);
+      },
+    });
+
+    // const rectangles = [];
+
+    await worker.loadLanguage('eng+ron');
+    await worker.initialize('eng+ron');
+
+    const {
+      data: { text },
+    } = await worker.recognize(documentBuffer);
+    console.log(text);
+    await worker.terminate();
+
+    return NextResponse.json({ success: true, data: {} });
+  } catch (error: any) {
+    return NextResponse.json({ success: false, data: null });
+  }
+}
---
+++ components/UploadForm/UploadForm.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useState } from 'react';
+
+export function UploadForm() {
+  const [data, setData] = useState('');
+  const [file, setFile] = useState<File>();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!file) {
+      return;
+    }
+
+    try {
+      const formData = new FormData();
+      formData.set('document', file);
+
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(await response.text());
+      }
+
+      console.log(response);
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='mb-24'>
+        <input
+          type='file'
+          name='document'
+          onChange={(e) => setFile(e.target.files?.[0])}
+        />
+        <input type='submit' value='Submit' />
+      </form>
+      <pre>{data}</pre>
+    </div>
+  );
+}
