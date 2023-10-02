a7b3a824b754c385842ab0271eb01febd758b8e4
xsuve <georgebaba99@yahoo.com>
2023-10-02 13:47:50 +0300

    Adjust navigation for process.

--- app/documente-necesare/[slug]/layout.tsx
+++ app/documente-necesare/[slug]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
-import { Heading, Logo } from '@/components/ui';
-import { ArrowLeftIcon } from '@heroicons/react/24/outline';
+import { Heading, Text, Logo } from '@/components/ui';
+import { ArrowLeftIcon, FlagIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { getProcess } from '@/utils/db';
 
@@ -25,15 +25,21 @@ export default function DocumenteNecesareSlugLayout({
   return (
     <main>
       <div className='px-24 pt-6 flex justify-between'>
-        <Link
-          href='/documente-necesare'
-          className='inline-flex items-center gap-x-4'
-        >
-          <ArrowLeftIcon className='w-6 h-6' />
-          <Heading type='section'>Procese</Heading>
-        </Link>
-        <Link href='/'>
-          <Logo />
+        <div className='flex items-center gap-x-12'>
+          <Link
+            href='/documente-necesare'
+            className='flex items-center gap-x-4'
+          >
+            <ArrowLeftIcon className='w-6 h-6' />
+            <Heading type='section'>Procese</Heading>
+          </Link>
+          <Link href='/'>
+            <Logo />
+          </Link>
+        </div>
+        <Link href='/contact' className='flex items-center gap-x-4'>
+          <FlagIcon className='w-6 h-6' />
+          <Text type='primary-bold'>Raportează o problemă</Text>
         </Link>
       </div>
       {children}
