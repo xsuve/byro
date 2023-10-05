c649c82e3075f8ddf36e712b3ee5413da7d35c9f
xsuve <georgebaba99@yahoo.com>
2023-10-05 13:30:38 +0300

    Adjust font-size for process date.

--- components/Process/Process.tsx
+++ components/Process/Process.tsx
@@ -48,7 +48,10 @@ export const Process: FC<ProcessProps> = ({
         <div className='flex items-center justify-between p-4 rounded-md ring-1 ring-inset ring-layout-border'>
           <div className='flex flex-col gap-y-1'>
             <Text type='tertiary'>Ultima actualizare</Text>
-            <Text type='primary-bold'>
+            <Text
+              type='primary-bold'
+              className='2xl:!text-sm xl:!text-sm text-xs'
+            >
               {DateTime.fromISO(updatedAt)
                 .setLocale('ro')
                 .toFormat('dd MMMM, yyyy')}
